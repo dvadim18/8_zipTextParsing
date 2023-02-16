e7412ac582b1f3e7939e54bd9ac972c185272b51
Vadim Dinislamov <dvadim87@gmail.com>
2023-02-16 21:43:09 +0300

    add sample.zip

---
+++ src/test/java/ZipTextParsingTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ZipTextParsingTest {
+
+    ClassLoader cl = ZipTextParsingTest.class.getClassLoader();
+
+    @Test
+    void zipParseTextTest() throws Exception {
+        try (
+                InputStream resource = cl.getResourceAsStream("sample.zip");
+                ZipInputStream zis = new ZipInputStream(resource);
+                )
+        {
+            ZipEntry entry;
+
+            while ((entry = zis.getNextEntry()) != null) {
+
+                assertThat(entry.getName().contains("12312312eaeqwsample.text"));
+
+            }
+
+        }
+    }
+}
